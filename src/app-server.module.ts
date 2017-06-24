f1ef9ddafa9440cf7de1b831488617ebeff6546b
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-23 23:36:53 -0500

    chore: generate typescript type declaration files (v0.2.0)

--- src/app-server.module.ts
+++ src/app-server.module.ts
@@ -3,7 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 
 import { BlogService, BLOG_PATH } from './services/blog.service';
 
-export function createAppServerModule<M, C>(appModule: Type<M>, appComponent: Type<C>, blogPath: string) {
+export function createAppServerModule<M, C>(appModule: Type<M>, appComponent: Type<C>, blogPath: string): Type<any> {
   @NgModule({
     imports: [
       appModule,
