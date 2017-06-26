869b50d2198157f276d6825e43fec86b9fb5c578
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-25 21:27:16 -0500

    refactor: add this type for blog loader function

--- src/webpack/blog-loader.ts
+++ src/webpack/blog-loader.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
+import * as webpack from 'webpack';
 
-module.exports = function blogLoader(blogPath: string) {
+module.exports = function blogLoader(this: webpack.loader.LoaderContext, blogPath: string) {
   this.addContextDependency(resolve(blogPath));
   return '';
 };
