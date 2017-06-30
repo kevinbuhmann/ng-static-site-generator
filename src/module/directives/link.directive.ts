8ee39bb6143099abff97cff23378146925637b85
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-30 18:28:08 -0500

    feat: add rel="noopener noreferrer" to external links

--- src/module/directives/link.directive.ts
+++ src/module/directives/link.directive.ts
@@ -3,19 +3,27 @@ import { Directive, HostBinding, Inject, Input, OnInit } from '@angular/core';
 import { ModuleOptions } from './../../options';
 import { MODULE_OPTIONS } from './../injection-tokens';
 
+const browser = typeof window !== 'undefined';
+
 @Directive({ selector: 'a' })
 export class LinkDirective implements OnInit {
   @Input() href: string;
+  @HostBinding('rel') rel: string;
   @HostBinding('target') target: string;
 
   constructor(@Inject(MODULE_OPTIONS) private moduleOptions: ModuleOptions) { }
 
   ngOnInit() {
-    if (this.moduleOptions.openExternalLinksInNewTab && this.target === undefined && this.href !== undefined && this.href.startsWith('http')) {
+    if (this.moduleOptions.openExternalLinksInNewTab && this.href !== undefined && this.href.startsWith('http')) {
+      this.rel = 'noopener noreferrer';
       this.target = '_blank';
     }
 
-    if (typeof window !== 'undefined' && this.target === undefined) {
+    if (browser && this.rel === undefined) {
+      this.rel = ''; // prevent browser from setting `rel="undefined"`
+    }
+
+    if (browser && this.target === undefined) {
       this.target = ''; // prevent browser from setting `target="undefined"`
     }
   }
